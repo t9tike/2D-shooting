c0cd5693ade87c0afa4ec62d25946acf4a6c65fa
t9tike <60396657+t9tike@users.noreply.github.com>
2021-03-16 22:17:40 +0200

    Update Valikko.cs
    Cleaned some code

--- Valikko.cs
+++ Valikko.cs
@@ -17,7 +17,7 @@ namespace Perkelesimu
         {
             InitializeComponent();
 
-            wplayer.URL = (@"C:\Users\keijo\Desktop\Windows ohjelmointi\Harjoittelua\Perkelesimu\Perkelesimu\Resources\Cowboy_Theme-Pavak-1711860633.wav");
+            wplayer.URL = (@"***");
             wplayer.controls.play();
             axWindowsMediaPlayer1.Hide();
         }
@@ -33,7 +33,7 @@ namespace Perkelesimu
         private void pictureBoxStart_MouseHover_1(object sender, EventArgs e)
         {
             pictureBoxStart.Image = Properties.Resources.start2;
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\keijo\Desktop\Windows ohjelmointi\Harjoittelua\StarMenu\Click On-SoundBible.com-1697535117.wav");
+            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"***");
             sound.Play();
         }
 
@@ -45,7 +45,7 @@ namespace Perkelesimu
         private void pictureBoxOptions_MouseHover_1(object sender, EventArgs e)
         {
             pictureBoxOptions.Image = Properties.Resources.settings2;
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\keijo\Desktop\Windows ohjelmointi\Harjoittelua\StarMenu\Click On-SoundBible.com-1697535117.wav");
+            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"***");
             sound.Play();
         }
 
@@ -57,7 +57,7 @@ namespace Perkelesimu
         private void pictureBoxQuit_MouseHover_1(object sender, EventArgs e)
         {
             pictureBoxQuit.Image = Properties.Resources.quit2;
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\keijo\Desktop\Windows ohjelmointi\Harjoittelua\StarMenu\Click On-SoundBible.com-1697535117.wav");
+            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"***");
             sound.Play();
         }
 
@@ -73,25 +73,12 @@ namespace Perkelesimu
 
         private void pictureBoxStart_Click_1(object sender, EventArgs e)
         {
-            //panelMenu.Hide();
-            Console.WriteLine("kaynnista painettu");
-
-            //formPelialue form1 = new formPelialue();
-            //form1.ShowDialog();
-
-
             formPelialue form2 = new formPelialue();
-
-
             wplayer.controls.stop();
             this.Hide();
-                
+              
             form2.Closed += (s, args) => this.Close();
-            form2.Show();
-            
-            
-
-            
+            form2.Show();   
         }
     }
 }
